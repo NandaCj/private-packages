10b644d0547d0f4d808487a2a6ae4db4a4d92446
NandaCj <nandha.paramasivam@gmail.com>
2020-05-20 13:39:34 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+# https://packaging.python.org/tutorials/packaging-projects/
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mathfn", # Replace with your own username
+    version="0.0.1",
+    author="Nanda",
+    author_email="nanda@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
